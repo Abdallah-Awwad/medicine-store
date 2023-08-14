c7994656dbbe0073f89aa4124a82782201301e25
Abdallah Awwad <abdallahhawwad@gmail.com>
2023-08-14 05:52:40 +0300

    created login page, finished my cart and some optimization

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var categoryButtons = document.querySelectorAll('.category-button');
+    let categoryButtons = document.querySelectorAll('.category-button');
     categoryButtons.forEach(function(button) {
         button.addEventListener('click', function() {
             categoryButtons.forEach(function(btn) {
                 btn.classList.remove('btn-outline-success');
                 btn.classList.remove('button');
+                btn.classList.remove('active-category');
             });
             this.classList.add('btn-outline-success');
             this.classList.add('button');
-            var category = this.getAttribute('data-category');
+            this.classList.add('active-category');
+            let category = this.getAttribute('data-category');
             if (category === 'all') {
-                var cards = document.querySelectorAll('#card-grid .col');
+                let cards = document.querySelectorAll('#card-grid .col');
                 cards.forEach(function(card) {
                     card.style.display = 'block';
                 });
             } else {
-                var cards = document.querySelectorAll('#card-grid .filter');
+                let cards = document.querySelectorAll('#card-grid .filter');
                 cards.forEach(function(card) {
-                    var cardCategory = card.getAttribute('data-category');
+                    let cardCategory = card.getAttribute('data-category');
                     if (cardCategory === category) {
                         card.style.display = 'block';
                     } else {
@@ -26,5 +28,50 @@ document.addEventListener('DOMContentLoaded', function() {
                 });
             }
         });
+    });
+    let addToCartButtons = document.querySelectorAll('.add-to-cart');
+    let items = {};
+    if (window.localStorage.getItem("items") != null) {
+        items = JSON.parse(window.localStorage.getItem("items"));
+    }
+    addToCartButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            let itemID = button.parentNode.parentNode.querySelector(".product-details").getAttribute("data-id");
+            if (! items[itemID]) {
+                items[itemID] = {
+                    name : button.parentNode.parentNode.querySelector(".product-name").innerHTML,
+                    price : button.parentNode.parentNode.querySelector(".product-price span").innerHTML,
+                    quantity: 1
+                }
+            } else {
+                items[itemID]["quantity"] = items[itemID]["quantity"] + 1
+            }
+            window.localStorage.setItem("items", JSON.stringify(items));
+
+            // adding success Alert
+            let successAlert = document.createElement("div");
+            successAlert.className = "item-added-alert container alert alert-success fixed-top text-center w-75 mt-1 py-2";
+            successAlert.setAttribute("role", "alert");
+            successAlert.innerHTML = '<i class="fa-regular fa-circle-check"></i> Item added successfully';
+            document.body.appendChild(successAlert);
+            
+            setTimeout(function() {
+                successAlert.classList.add("show");
+            }, 0);
+              
+            setTimeout(function() {
+                successAlert.classList.remove("show");
+                setTimeout(function() {
+                    successAlert.remove();
+                }, 6000);
+            }, 3000);              
+        });
+    });
+});
+
+$(function () {
+    $(document).scroll(function () {
+        let $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 });
